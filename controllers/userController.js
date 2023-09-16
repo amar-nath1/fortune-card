ab5f43834c203e14002c1be011cffb0676620cbc
amar-nath1 <amarat279955>
2023-09-17 01:06:32 +0530

    user password in no longer at risk. bcrypted

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,17 @@
+const bcrypt=require('bcrypt')
 const User=require('../models/user')
 exports.postUser=(req,res,next)=>{
 console.log(req.body,'reqbody')
+bcrypt.hash(req.body.password,10,(err,hash)=>{
+if (err){
+    console.error(err)
+    return
+}
 
 User.create({
     name:req.body.name,
     email:req.body.email,
-    password:req.body.password
+    password:hash
 }).then((postres)=>{
 res.status(200).json({'added':postres})
 }).catch((err)=>{
@@ -13,6 +19,9 @@ res.status(200).json({'added':postres})
         res.json({'errType':'already exists'})
     }
 })
+
+})
+
 }
 
 exports.userLogin=(req,res,next)=>{
@@ -24,15 +33,29 @@ exports.userLogin=(req,res,next)=>{
                     console.log(foundUser[0]['password'],'fu')
                     if(foundUser.length===0){
                         res.status(200).json({'auth':'nullexistence'})
+                    }
+                    else{
+                        bcrypt.compare(req.body.password,foundUser[0]['password'],(err,result)=>{
+                                if (err){
+                                    console.error(err)
+                                }
+                                if (result===true){
+                                    res.status(200).json({'auth':true})
+                                }
+                                else{
+                                    res.status(200).json({'auth':false})
+                                }
+                        })
                     }       
-         else if(foundUser[0]['password']===req.body.password){
-                res.status(200).json({'auth':true})
-            }
-            else{
-                res.status(200).json({'auth':false})
-            }
-        }).catch((err)=>{
+        //  else if(foundUser[0]['password']===req.body.password){
+        //         res.status(200).json({'auth':true})
+        //     }
+        //     else{
+        //         res.status(401).json({'auth':false})
+        //     }
+        // })
+    
+    }).catch((err)=>{
             res.status(200).json({'auth':'nullexistence'})
         })
-    
-    }
+}
